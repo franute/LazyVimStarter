1d48b342879492ab1e7c9a61237743695341c51f
Francisco Torres Pérez <259762-franute@users.noreply.gitlab.com>
2023-08-02 09:22:08 +0200

    Setting dracula as the main theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "catppuccin/nvim", lazy = true, name = "catppuccin" },
   { "EdenEast/nightfox.nvim" },
+  { "Mofiqul/dracula.nvim", lazy = true },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      colorscheme = "dracula",
     },
   },
 }
